95dbdc01cba80eccf318bff9c9733de595be3959
Артём Журавлёв <artembestnet@mail.ru>
2019-07-26 17:42:31 +0300

    Добавил новые функции

---
+++ OnlyDPDFull/HermesLocation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyDPDFull
+{
+    public class HermesLocationC
+    {
+
+        internal static refHermesLoc.Preadvice[] FillPreadvice()
+        {
+            refHermesLoc.Preadvice[] preadvices = new refHermesLoc.Preadvice[1];
+            
+            preadvices[0] = new refHermesLoc.Preadvice()
+            {   ///Идентификатор бизнес-юнита клиента
+                BusinessUnitCode = "1376",
+                ///Валюта стоимости посылки (Доступны только рубли)
+                CashOnDeliveryCurrency = "RUB",
+                ///Сумма, которую необходимо внести получателю при получении посылки. Если посылка предоплачена полностью, значение установить = 0, если частично, указать, сколько необходимо внести покупателю.
+                CashOnDeliveryValue = 1000,
+                //Номер заказа в системе клиента, используется для дальнейшего трекинга, отчетности и т.д.
+                ClientOrderNumber = "1234567890",//не обяз
+                                                 //Уникальный номер посылки в системе клиента (может быть равен ParcelBarcode, если генерация штрих-кода посылки идет на стороне клиента). 
+                ClientParcelNumber = "12345678901234",
+                ///Код страны
+                CustomerCountryCode = "RUS",
+                ///Фамилия получателя
+                CustomerSurname = "Иванов",
+
+                CustomerMobilePhoneNumber = "8-903-123-44-55",
+
+                Services = new string[] { "DIRECT_DELIVERY" },
+                //Валюта страховой стоимости посылки (Доступны только рубли)
+                InsuranceCurrency = "RUB",
+                ///Страховая стоимость посылки. Если посылка предоплачена, значение установить = реальной стоимости посылки
+                InsuranceValue = 1000,
+                //Номер ПВЗ
+                ParcelshopCode = "900176"
+            };
+            return preadvices;
+        }
+        /// <summary>
+        /// Данный метод предназначен для первоначальной передачи информации о посылках. 
+        /// </summary>
+        /// <param name="client"></param>
+        public static void CreatePreadvice(refHermesLoc.WebServiceClient client)
+        {
+           
+            
+
+        }
+
+        internal static void UpdatePreadvice(refHermesLoc.WebServiceClient client)
+        {
+
+        }
+    }
+
+   
+}
--- OnlyDPDFull/Program.cs
+++ OnlyDPDFull/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
+using OnlyDPDFull.refHermesLoc;
 
 namespace OnlyDPDFull
 {
@@ -16,7 +17,7 @@ namespace OnlyDPDFull
             Program a = new Program();
             //  a.DpdLocation();
             //  a.DpdLocationHTTP();
-            //a.HermesLocation();
+            a.HermesLocation();
             a.HermesLocationHTTP();
 
         }
@@ -26,9 +27,43 @@ namespace OnlyDPDFull
             refHermesLoc.WebServiceClient client = new refHermesLoc.WebServiceClient();
             client.ClientCredentials.UserName.UserName = "testlogin";
             client.ClientCredentials.UserName.Password = "testpassword";
-            var result = client.GetParcelShops("1000");
 
 
+            //var result = client.GetParcelShops("1000");
+            /////Данный метод предназначен для первоначальной передачи информации о посылках. Передача информации о посылках возможна и без указания штрих-кодов посылок. В этом случае генерация номеров будет происходить на стороне Hermes Russia. Эта опция не включена по умолчанию, поэтому для ее активации необходимо связаться с отделом продаж Hermes Russia.
+            //var preadvices =  HermesLocationC.FillPreadvice();
+            //var result2 = client.CreatePreadvice(preadvices);
+
+            /////Данный метод позволяет обновлять информацию о еще не переданных на доставку посылках. Обновление информации производится по совпадению клиентского номера посылки. При совпадении клиентского номера посылки все остальные поля будут обновлены данными из запроса.
+            //var preadvices3 = HermesLocationC.FillPreadvice();
+            //var result3 = client.UpdatePreadvice(preadvices3);
+
+            /////Данный метод позволяет удалять еще не переданные на доставку посылки, если необходимость в их доставке отпала или данные были ошибочными. Удаление информации о посылках происходит по совпадению штрих-кодов.
+            //string[] parcelBarcodes = new string[] { "12345678901234", "12345678901235" };
+            //var resule4 = client.DeletePreadvice(parcelBarcodes);
+
+            /////Данный метод позволяет получить список всех еще не переданных на доставку посылок заданного бизнес-юнита.
+            string businessUnitCode = "1000";
+            //var resule5 = client.GetPreadvices(businessUnitCode);
+
+            PreadviceResult[] resule6 = client.SendAllPreadvicesToDelivery(businessUnitCode);
+
+
+            /////Данный метод предназначен для решения задачи получения всех статусов всех посылок заданного бизнес-юнита в режиме, приближенном к реальному времени и сохранении статусов в системе клиента.
+            /////Метод позволяет получить все статусы, которые появились в системе Hermes Russia с заданной даты.
+            /////Алгоритм работы с методом
+            ////1.При первом вызове необходимо передать в параметре dateFrom текущую дату
+            ////2.Далее гарантируется передача всех статусов без потерь или повторов, при условии, что параметр dateFrom будет содержать значение, возвращенное в параметре NextRequestDateFrom из предыдущего вызова метода, а параметр dateTo необходимо оставлять пустым
+            ////3.При удалении статуса, он передается еще раз, но уже с заполненным полем StatusDeleteTimestamp
+            //DateTime dateTime = new DateTime(2015, 7, 20, 18, 30, 25);
+
+            //StatusDateRangeResult resule7 = client.GetStatusesByBusinessUnit(businessUnitCode, dateTime , null);
+            //dateTime = resule7.NextRequestDateFrom;
+
+            ///Данный метод предназначен для получения всех статусов по заданному списку посылок. Удаленные статусы не возвращаются данным методом.
+            ///Часто использовать этот метод крайне не рекомендуется, только для редких и частных случаев.
+            string[] parcelBarCodes = new string[1] { "20000000000001" };
+            client.GetStatusesByParcelBarcodes(parcelBarCodes);
         }
 
         private void HermesLocationHTTP()
@@ -61,10 +96,26 @@ namespace OnlyDPDFull
 
         private void DpdLocation()
         {
+
+            //string delisId = "exampleUserID";
+            //string password = "examplePassword";
+            //string messageLanguage = "en_US"; // optional
+            //string authToken = "";
+
+            //// create a client object
+            //LoginService.LoginService loginServiceClient = new LoginService.LoginService();
+
+            //// create a login object and provide the delisId and password
+            //LoginService.Login login = new LoginService.Login();
+            //login = loginServiceClient.getAuth(delisId, password, messageLanguage);
+
+            //// get the token
+            //authToken = login.authToken;
+
             refDpdLoc.DPDGeography2Client client = new refDpdLoc.DPDGeography2Client();
             refDpdLoc.dpdCitiesCashPayRequest request = new refDpdLoc.dpdCitiesCashPayRequest();
-            client.ClientCredentials.UserName.UserName = "";
-            client.ClientCredentials.UserName.Password = "";
+            client.ClientCredentials.UserName.UserName = "exampleUserID";
+            client.ClientCredentials.UserName.Password = "examplePassword";
             
             // proxy.ClientCredentials.UserName.Password = "testpassword"; " 
             request.auth = new refDpdLoc.auth();
@@ -78,7 +129,7 @@ namespace OnlyDPDFull
         private void DpdLocationHTTP()
         {
             // string _url = "http://dpd.ru/ws/geography/2015-05-20/DPDGeography2";
-            string _url = "http://wstest.dpd.ru/services/geography2?wsdl";
+            string _url = "https://api.hermes-dpd.ru/WS";
             string _soapEnvelope = File.ReadAllText(@"getCitiesCashPay.xml"); // SOAP-конверт (запрос), который будет отправлен к API
 
             WebRequest _request = HttpWebRequest.Create(_url);
@@ -89,7 +140,7 @@ namespace OnlyDPDFull
             _request.ContentLength = _soapEnvelope.Length;
 
             // _request.Headers.Add("SOAPAction", "http://dpd.ru/ws/geography/2015-05-20/DPDGeography2/getCitiesCashPayRequest");
-            _request.Headers.Add("SOAPAction", "http://wstest.dpd.ru/services/geography2?getCitiesCashPayRequest");
+            _request.Headers.Add("SOAPAction", "https://api.hermes-dpd.ru/WS/IWebService/GetParcelShops");
             // _request.Headers.Add(_url + @"/" + _method);
             // пишем тело
             StreamWriter _streamWriter = new StreamWriter(_request.GetRequestStream());
